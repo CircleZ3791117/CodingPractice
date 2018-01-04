b386de3227308dbb2d8a92c21f521067de4bce34
CircleZ3791117 <zhouhuan@onescorpion.com>
2018-01-04 15:15:41 +0800

    leetcode practice(292,693)

---
+++ 693_BinaryNumberWithAlternatingBits_easy.py
@@ -0,0 +1,46 @@
+# !/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+__author__ = 'circlezhou'
+
+'''
+Description:
+Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+
+Example 1:
+Input: 5
+Output: True
+Explanation:
+The binary representation of 5 is: 101
+Example 2:
+Input: 7
+Output: False
+Explanation:
+The binary representation of 7 is: 111.
+Example 3:
+Input: 11
+Output: False
+Explanation:
+The binary representation of 11 is: 1011.
+Example 4:
+Input: 10
+Output: True
+Explanation:
+The binary representation of 10 is: 1010.
+'''
+class Solution:
+	def hasAlternatingBits(self, n):
+		"""
+		:type n: int
+		:rtype: bool
+		"""
+		binary_n = bin(n)[2:]
+		for i in range(len(binary_n)-1):
+			if binary_n[i] == binary_n[i+1]:
+				return False
+		return True
+
+
+
+
+
