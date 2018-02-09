b8b10edc605303a18a42eec467d008d7114ec51c
CircleZ3791117 <zhouhuan@onescorpion.com>
2018-02-09 10:21:28 +0800

    leetcode practice(371)

--- 371_SumOfTwoIntegers_easy.py
+++ 371_SumOfTwoIntegers_easy.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__author__ = 'circlezhou'
+
 '''
 Description:
 
@@ -12,7 +14,6 @@ Given a = 1 and b = 2, return 3.
 Credits:
 Special thanks to @fujiaozhu for adding this problem and creating all test cases.
 '''
-from operator import xor
 
 class Solution:
 	def getSum(self, a, b):
@@ -21,16 +22,117 @@ class Solution:
 		:type b: int
 		:rtype: int
 		"""
-		result = []
-		bin_a_reverse = bin(a)[2:][::-1]
-		bin_b_reverse = bin(b)[2:][::-1]
-		
-		
-	
+		# get the binary code of a and b, and then reverse binary code
+		if a >= 0 and b>= 0:  
+			re_bin_a = list(bin(a)[2:][::-1])
+			re_bin_b = list(bin(b)[2:][::-1])
+			size = max(len(re_bin_a), len(re_bin_b))
+			if len(re_bin_a) > len(re_bin_b):
+				re_bin_b.extend(['0']*(len(re_bin_a) - len(re_bin_b)))
+			else:
+				re_bin_a.extend(['0']*(len(re_bin_b) - len(re_bin_a)))
+			print('re_bin_a:{}, re_bin_b:{}'.format(re_bin_a, re_bin_b))
+			result = []
+			tmp = '0'
+			for i in range(size):
+				if tmp == '0' and (re_bin_a[i] == '0' and re_bin_b[i] == '0'):
+					result.append('0')
+					print('1.result.append(0)')
+					tmp = '0'
+					continue
+				if tmp == '0' and ((re_bin_a[i] == '1' and re_bin_b[i] == '0') or (re_bin_a[i] == '0' and re_bin_b[i] == '1')):
+					result.append('1')
+					print('2.result.append(1)')
+					tmp = '0'
+					continue
+				if tmp == '0' and (re_bin_a[i] == '1' and re_bin_b[i] == '1'):
+					result.append('0')
+					print('3.result.append(0)')
+					tmp = '1'
+					continue
+				if tmp == '1' and (re_bin_a[i] == '0' and re_bin_b[i] == '0'):
+					result.append('1')
+					print('4.result.append(1)')
+					tmp = '0'
+					continue
+				if tmp == '1' and ((re_bin_a[i] == '1' and re_bin_b[i] == '0') or (re_bin_a[i] == '0' and re_bin_b[i] == '1')):
+					result.append('0')
+					print('5.result.append(0)')
+					tmp = '1'
+					continue
+				if tmp == '1' and (re_bin_a[i] == '1' and re_bin_b[i] == '1'):
+					result.append('1')
+					print('6.result.append(1)')
+					tmp = '1'
+					continue
+			if tmp == '1':
+				result.append(tmp)
+			print(result)
+			new_result = ''.join(result[::-1])
+			print('new_result is : {}'.format(new_result))
+			final_result = int(new_result, 2)
+			return final_result
+		elif a < 0  and b < 0:
+			a, b = abs(a), abs(b)
+			re_bin_a = list(bin(a)[2:][::-1])
+			re_bin_b = list(bin(b)[2:][::-1])
+			size = max(len(re_bin_a), len(re_bin_b))
+			if len(re_bin_a) > len(re_bin_b):
+				re_bin_b.extend(['0']*(len(re_bin_a) - len(re_bin_b)))
+			else:
+				re_bin_a.extend(['0']*(len(re_bin_b) - len(re_bin_a)))
+			print('re_bin_a:{}, re_bin_b:{}'.format(re_bin_a, re_bin_b))
+			result = []
+			tmp = '0'
+			for i in range(size):
+				if tmp == '0' and (re_bin_a[i] == '0' and re_bin_b[i] == '0'):
+					result.append('0')
+					print('1.result.append(0)')
+					tmp = '0'
+					continue
+				if tmp == '0' and ((re_bin_a[i] == '1' and re_bin_b[i] == '0') or (re_bin_a[i] == '0' and re_bin_b[i] == '1')):
+					result.append('1')
+					print('2.result.append(1)')
+					tmp = '0'
+					continue
+				if tmp == '0' and (re_bin_a[i] == '1' and re_bin_b[i] == '1'):
+					result.append('0')
+					print('3.result.append(0)')
+					tmp = '1'
+					continue
+				if tmp == '1' and (re_bin_a[i] == '0' and re_bin_b[i] == '0'):
+					result.append('1')
+					print('4.result.append(1)')
+					tmp = '0'
+					continue
+				if tmp == '1' and ((re_bin_a[i] == '1' and re_bin_b[i] == '0') or (re_bin_a[i] == '0' and re_bin_b[i] == '1')):
+					result.append('0')
+					print('5.result.append(0)')
+					tmp = '1'
+					continue
+				if tmp == '1' and (re_bin_a[i] == '1' and re_bin_b[i] == '1'):
+					result.append('1')
+					print('6.result.append(1)')
+					tmp = '1'
+					continue
+			if tmp == '1':
+				result.append(tmp)
+			print(result)
+			new_result = ''.join(result[::-1])
+			print('new_result is : {}'.format(new_result))
+			final_result = int(new_result, 2)
+			return (-1) * final_result
+		else:
+			if a > b:
+				return a-abs(b)
+			else:
+				return b-abs(a)
 
 
 
 
+# test
 s = Solution()
-s.getSum(6, 2)
+print(s.getSum(1, -1))
+
 
