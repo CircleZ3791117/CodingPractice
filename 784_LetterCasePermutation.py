3d42b65e1624cb97576614aa2bda530890b362ad
CircleZ3791117 <zhouhuan@onescorpion.com>
2018-03-06 11:40:33 +0800

    leetcode practice(784)

--- 784_LetterCasePermutation.py
+++ 784_LetterCasePermutation.py
@@ -29,18 +29,42 @@ class Solution:
 		:type S: str
 		:rtype: List[str]
 		"""
-		result = []
-		if S is None:
-			return result
-		result.append(S)
-		tmp = str()
-		for i in S:
-			if ord(i) >= 65 and ord(i) <= 90:
-				tmp += chr(ord(i) + 32)
-			if ord(i) >= 97 and ord(i) <= 122:
-				tmp += chr(ord(i) - 32)
-			tmp += i
-		result.append(tmp)
-		return result
+		ans = [[]]
+		for letter in S:
+			n = len(ans)
+			if letter.isalpha():
+				for i in xrange(n):
+					ans.append(ans[i][:])
+					ans[i].append(letter.lower())
+					ans[i+n].append(letter.upper())
+			else:
+				for i in xrange(n):
+					ans[i].append(letter)
+		return map("".join, ans)
+
+'''
+Complexity Analysis
+
+Time Complexity: O(2^N * N), where N is the length of S. This reflects the cost of writing the answer.
+
+Space Complexity: O(2^N * N).
+'''
+
+'''
+Intuition
+
+Maintain the correct answer as we increase the size of the prefix of S we are considering.
+
+For example, when S = "abc", maintain ans = [""], and update it to ans = ["a", "A"], ans = ["ab", "Ab", "aB", "AB"], ans = ["abc", "Abc", "aBc", "ABc", "abC", "AbC", "aBC", "ABC"] as we consider the letters "a", "b", "c".
+
+Algorithm
+
+If the next character c is a letter, then we will duplicate all words in our current answer, and add lowercase(c) to every word in the first half, and uppercase(c) to every word in the second half.
+
+If instead c is a digit, we'll add it to every word.
+'''
+
+
+
 
 
