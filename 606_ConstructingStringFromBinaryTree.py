9c7ae3e77ccafc173bfb0342880f61b96504f13a
CircleZ3791117 <zhouhuan@onescorpion.com>
2018-04-09 17:49:24 +0800

    leetcode practice(606)

--- 606_ConstructingStringFromBinaryTree.py
+++ 606_ConstructingStringFromBinaryTree.py
@@ -41,13 +41,18 @@ Solution
 '''
 class TreeNode(object):
 	def __init__(self, x):
-		self.value = x
+		self.val = x
 		self.left = None
 		self.right = None
 
 class Solution(object):
-	def tree2str(self, t):
+  def tree2str(self, t):
 		"""
 		:type t: TreeNode
 		:rtype: str
 		"""
+    if not t: return ''
+    left = '({})'.format(self.tree2str(t.left)) if (t.left or t.right) else ''
+    right = '({})'.format(self.tree2str(t.right)) if t.right else ''
+    return '{}{}{}'.format(t.val, left, right)
+
