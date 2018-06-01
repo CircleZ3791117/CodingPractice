9346533a28c4855ff6f28976d50b6becef3aa4d5
CircleZ3791117 <zhouhuan@onescorpion.com>
2018-06-01 15:21:43 +0800

    Leetcode practice(217)

---
+++ 217_ContainsDuplicate.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'circlezhou'
+
+'''
+Description:
+
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+'''
+
+class Solution(object):
+	def containsDuplicate(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: bool
+		"""
+		value_map = {}
+		for i in nums:
+			if i not in value_map.keys():
+				value_map[i] = 1
+			else:
+				return True
+		return False
+
