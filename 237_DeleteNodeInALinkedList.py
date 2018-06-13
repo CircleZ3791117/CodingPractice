ef2e8606534df8282221bd6f463319141d247074
CircleZ3791117 <zhouhuan@onescorpion.com>
2018-06-13 17:11:14 +0800

    Leetcode practice(237)

--- 237_DeleteNodeInALinkedList.py
+++ 237_DeleteNodeInALinkedList.py
@@ -29,3 +29,21 @@ All of the nodes' values will be unique.
 The given node will not be the tail and it will always be a valid node of the linked list.
 Do not return anything from your function.
 '''
+
+
+# definition for singly linked list
+class ListNode(object):
+	def __init__(self, x)
+		self.val = x
+		self.next = None
+
+class Solution(object):
+	def deleteNode(self, node):
+		"""
+		:type node: ListNode
+		:rtype: void Do not return anything, modify node in-place instead
+		"""
+		node.val = node.next.val
+		node.next = node.next.next
+
+
