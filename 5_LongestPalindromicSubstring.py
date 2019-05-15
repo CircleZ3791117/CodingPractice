25999e3dcc8813cb81ff042d2a77978aa72ec48a
circlez3791117 <zhouhuan@onescorpion.com>
2019-05-15 13:46:18 +0800

    LC 5

---
+++ 5_LongestPalindromicSubstring.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Description:
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: "cbbd"
+Output: "bb"
+"""
