2aa00d29dc21384711cc6e290d38d554ceda9cb2
circlez3791117 <zhouhuan@onescorpion.com>
2019-05-27 11:40:01 +0800

    LC 8

---
+++ 8_StringToInteger.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Description:
+
+Implement atoi which converts a string to an integer.
+
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned.
+
+Note:
+
+Only the space character ' ' is considered as whitespace character.
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
+Example 1:
+
+Input: "42"
+Output: 42
+Example 2:
+
+Input: "   -42"
+Output: -42
+Explanation: The first non-whitespace character is '-', which is the minus sign.
+             Then take as many numerical digits as possible, which gets 42.
+Example 3:
+
+Input: "4193 with words"
+Output: 4193
+Explanation: Conversion stops at digit '3' as the next character is not a numerical digit.
+Example 4:
+
+Input: "words and 987"
+Output: 0
+Explanation: The first non-whitespace character is 'w', which is not a numerical 
+             digit or a +/- sign. Therefore no valid conversion could be performed.
+Example 5:
+
+Input: "-91283472332"
+Output: -2147483648
+Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
+             Thefore INT_MIN (−231) is returned.
+"""
+
+class Solution:
+	def myAtoi(self, s: str) -> int:
+		if len(s) == 0:
+			return 0
+		number_stack = []
+		non_ws_index = 0
+		for i in range(len(s)):
+			if s[i] == ' ':
+				non_ws_index += 1
+			else:
+				break
+		if non_ws_index < len(s):
+			s = s[non_ws_index:]
+		else:
+			return 0
+		if len(s) == 1:
+			if ord('0') <= ord(s[0]) <= ord('9'):
+				return int(s[0])
+			else:
+				return 0
+		flag = 1
+		if s[0] == "-":
+			flag = -1
+			s = s[1:]
+		elif s[0] == "+":
+			s = s[1:]
+		for i in range(len(s)):
+			if ord('0') <= ord(s[i]) <= ord('9'):
+				number_stack.append(s[i])
+			else:
+				break
+		if len(number_stack) == 0:
+			return 0
+		ns_len = len(number_stack)
+		result = 0
+		for i in range(ns_len):
+			result += int(number_stack[i]) * 10**(ns_len-i-1)
+		result = result * flag
+		if result > 2**31 - 1:
+			return 2**31 - 1
+		elif result < -2**31:
+			return -2**31
+		else:
+			return result
