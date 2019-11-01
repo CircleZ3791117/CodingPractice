10201aec2b652e4dbefde3d0a5ba733bbac811de
circlez3791117 <zhouhuan@onescorpion.com>
2019-11-01 17:24:53 +0800

    Using recursive method.

--- source_code/LC10_RegularExpressionMatch.py
+++ source_code/LC10_RegularExpressionMatch.py
@@ -54,20 +54,13 @@ Output: false
 
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        i = 0
-        j = 0
-        while i < len(s) and j < len(p):
-            if s[i] == p[j] or p[j] == '.':
-                i += 1
-                if j + 1 < len(p) and p[j + 1] != '*':
-                    j += 1
-                if j + 1 == len(p):
-                    j += 1
-            else:
-                if j + 1 < len(p) and p[j + 1] == '*':
-                    j += 2
-        if i == len(s):
-            if j == len(p) or (j == len(p) - 2 and p[j + 1] == '*'):
-                return True
+        if not p:
+            return not s
+
+        # first match
+        first_match = bool(s) and p[0] in [s[0], '.']
+
+        if len(p) >= 2 and p[1] == '*':
+            return self.isMatch(s, p[2:]) or first_match and self.isMatch(s[1:], p)
         else:
-            return False
+            return first_match and self.isMatch(s[1:], p[1:])
