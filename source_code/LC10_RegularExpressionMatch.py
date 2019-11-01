ab10ed99d2eea34cbdf2c053e3185b19c41bb4f9
circlez3791117 <zhouhuan@onescorpion.com>
2019-11-01 11:28:19 +0800

    Change directory for souce code.

--- source_code/LC10_RegularExpressionMatch.py
+++ source_code/LC10_RegularExpressionMatch.py
@@ -3,7 +3,6 @@
 
 __author__ = 'CZ'
 
-
 """
 Description:
 Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
@@ -52,59 +51,23 @@ p = "mis*is*p*."
 Output: false
 """
 
+
 class Solution:
-	def isMatch(self, s: str, p: str) -> bool:
-		i = 0
-		j = 0
-		while i < len(s) and j < len(p):
-			if s[i] == p[j] or p[j] == '.':
-				i += 1
-				if j+1 < len(p) and p[j+1] != '*':
-					j += 1
-				if j+1 >= len(p):
-					j += 1
-			else:
-				if j+1 < len(p) and p[j+1] == '*':
-					j += 2
-		if i == len(s):
-			return True
-		else:
-			return False
-
-
-import unittest
-class SolutionTestCase(unittest.TestCase):
-
-	def __init__(self, *args, **kwargs):
-		super(SolutionTestCase, self).__init__(*args, *kwargs)
-		self.test_solution = Solution()
-
-	@classmethod
-	def setUpClass(cls):
-		print("setUpClass")
-
-	@classmethod
-	def tearDownClass(cls):
-		print("tearDownClass")
-
-	def setUp(self):
-		print("--setUp--")
-
-	def tearDown(self):
-		print("--tearDown--")
-
-	def test_isMatch(self):
-		print("test isMatch()...")
-		self.assertEqual(False, self.test_solution.isMatch('aa', 'a'))
-		self.assertEqual(True, self.test_solution.isMatch('aa', 'aa'))
-
-
-if __name__ == "__main__":
-	unittest.main()
-
-
-			
-
-
-
-
+    def isMatch(self, s: str, p: str) -> bool:
+        i = 0
+        j = 0
+        while i < len(s) and j < len(p):
+            if s[i] == p[j] or p[j] == '.':
+                i += 1
+                if j + 1 < len(p) and p[j + 1] != '*':
+                    j += 1
+                if j + 1 == len(p):
+                    j += 1
+            else:
+                if j + 1 < len(p) and p[j + 1] == '*':
+                    j += 2
+        if i == len(s):
+            if j == len(p) or (j == len(p) - 2 and p[j + 1] == '*'):
+                return True
+        else:
+            return False
