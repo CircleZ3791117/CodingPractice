932b1d9e6d4b9195c66c41dd357db50ab815df7e
circlez3791117 <zhouhuan@onescorpion.com>
2019-11-28 15:48:27 +0800

    Add DP solution.

--- source_code/LC95_UniqueBinarySearchTreesII.py
+++ source_code/LC95_UniqueBinarySearchTreesII.py
@@ -28,28 +28,53 @@ from utils.construct_tree import TreeNode
 
 
 class Solution:
-    def get_trees_recur(self, left, right):
-        node_list = list()
-        if left > right:
-            return [None]
-        if left == right:
-            return [TreeNode(left)]
-        i = left
-        while i <= right:
-            left_node_list = self.get_trees_recur(left, i - 1)
-            right_node_list = self.get_trees_recur(i + 1, right)
-            for lnode in left_node_list:
-                for rnode in right_node_list:
-                    root = TreeNode(i)
-                    root.left = lnode
-                    root.right = rnode
-                    node_list.append(root)
-            i += 1
-        return node_list
-
     def generateTrees(self, n: int) -> list:
         if n < 1:
             return
-        return self.get_trees_recur(1, n)
 
+        def get_trees_recur(left, right):
+            node_list = list()
+            if left > right:
+                return [None]
+            i = left
+            while i <= right:
+                left_node_list = get_trees_recur(left, i - 1)
+                right_node_list = get_trees_recur(i + 1, right)
+                for lnode in left_node_list:
+                    for rnode in right_node_list:
+                        root = TreeNode(i)
+                        root.left = lnode
+                        root.right = rnode
+                        node_list.append(root)
+                i += 1
+            return node_list
 
+        return get_trees_recur(1, n)
+
+    # Using DP
+    def generate_trees_using_dp(self, n: int) -> list:
+        if n < 1:
+            return [None]
+
+        # change node value by delta deeply
+        def deep_copy(node, delta):
+            if not node:
+                return None
+            delta_node = TreeNode(node.val + delta)
+            delta_node.left = deep_copy(node.left, delta)
+            delta_node.right = deep_copy(node.right, delta)
+            return delta_node
+
+        # define dp, each element represent the root list of all possible BST tree
+        dp = [[] for _ in range(n + 1)]
+        dp[0].append(None)
+        for i in range(1, n + 1):
+            for j in range(0, i):
+                for lnode in dp[j]:
+                    for rnode in dp[i - 1 - j]:
+                        root = TreeNode(j + 1)
+                        root.left = lnode
+                        root.right = deep_copy(rnode, j + 1)
+                        dp[i].append(root)
+
+        return dp[n]
